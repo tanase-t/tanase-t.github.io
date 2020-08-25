885717ca24f00f6a8b773a5fd5df3adc5fc25d5b
(T.T) <tanase@engraphia.com>
2020-08-25 18:52:58 +0900

    initial commit

---
+++ dist/precache-manifest.81a78ba01ac99502385d170c4cbed843.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35aae5c509166e2a5ef0",
+    "url": "/css/app.e7f571f9.css"
+  },
+  {
+    "revision": "e60976515fc41a586156",
+    "url": "/css/chunk-vendors.cc646ac6.css"
+  },
+  {
+    "revision": "f76fe84f5b6e124eaf80cee407497ec2",
+    "url": "/img/drink_nodo_kawaita_man.f76fe84f.png"
+  },
+  {
+    "revision": "83988e1f62720e27e740bd9781fa61de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35aae5c509166e2a5ef0",
+    "url": "/js/app.6e5cee3a.js"
+  },
+  {
+    "revision": "e60976515fc41a586156",
+    "url": "/js/chunk-vendors.890a9206.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
